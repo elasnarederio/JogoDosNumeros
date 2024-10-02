96c46df4bd70870dd822076e45868f008313b7f3
elasnarederio <elasnarederio@gmail.com>
2024-10-02 16:30:19 -0300

    criando funções

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ function gerarNumeroAleatorio(){
         }, 2000);
     });
 }
+
+function verificarPalpite(palpite){
+    return new Promise((resolve, reject) => {
+        tentativas++;
+        if(palpite < 1 || palpite >100){
+            reject("Por favor, digite um número entre 1 e 100");
+        }else if(palpite === numeroSecreto){
+            resolve("Parabéns! Você acertou o número!");
+        }else if(palpite < numeroSecreto){
+            resolve("Muito baixo! Jogue novamente!");
+        }else {
+            resolve("Muito alto! Jogue novamente!");
+        }
+    });
+}
+
+async function iniciarJogo(){
+    await gerarNumeroAleatorio();
+    const botaoEnviar = document.getElementById("enviar");
+    const inputPalpite = document.getElementById("palpite");
+    const resultado = document.getElementById("resultado");
+    const tentativasDisplay = document.getElementById("tentativas");
+}
