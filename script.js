a9483742fc8f20303884cad9dc98342241332834
elasnarederio <elasnarederio@gmail.com>
2024-10-02 16:53:53 -0300

    salvando por desencargo

--- script.js
+++ script.js
@@ -32,4 +32,18 @@ async function iniciarJogo(){
     const inputPalpite = document.getElementById("palpite");
     const resultado = document.getElementById("resultado");
     const tentativasDisplay = document.getElementById("tentativas");
+
+    botaoEnviar.addEventListener("click", async() => {
+        try {
+            const palpite = parseInt(inputPalpite.value);
+            const mensagem = await verificarPalpite(palpite);
+            resultado.textContent = mensagem;
+            tentativasDisplay.textContent = `Tentativas: ${tentativas}`;
+
+            inputPalpite.value= "";
+                        
+        } catch (erro) {
+            resultado.textContent = erro;            
+        }
+    });
 }
